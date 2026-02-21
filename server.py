498e07ba57f3653e265126f1e1c68f651b0152a3
Sam <samueldmoores@gmail.com>
2026-02-21 14:15:25 -0800

    fixed true to True

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print(f"Connected by {addr}")
-        while true:
+        while True:
             data = conn.recv(1024)
             if not data: break
             conn.sendall(data.upper())
