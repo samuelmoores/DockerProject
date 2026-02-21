a616bd4e96e0528dbd403010b36100ead8d55e55
Sam <samueldmoores@gmail.com>
2026-02-21 14:05:40 -0800

    server script

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = '0.0.0.0'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Server listening on {HOST}:{PORT}")
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while true:
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data.upper())
