979f8be8d94dce7ad4d90c5bda0eba26b443049b
Sam <samueldmoores@gmail.com>
2026-02-22 15:08:08 -0800

    client server communication

--- Client/client.py
+++ Client/client.py
@@ -1,13 +1,21 @@
-import socket
+import socket, time
 
-TCP_IP = '127.0.0.1' #Localhost
+TCP_IP = "server"
 TCP_PORT = 65432
-MESSAGE = "Hello, World!" #The message to send
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+s = None
+for attempt in range(5):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((TCP_IP, TCP_PORT))
+        print("Connected!")
+        break
+    except ConnectionRefusedError:
+        print("Server not ready, retrying...")
+        time.sleep(2)
+
 s.send(b'Hi!')
 data = s.recv(1024)
 s.close()
- 
+
 print("received data:", data)
